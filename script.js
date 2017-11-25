cee3ead4ee82d5420b408afd9a23b8c8641d8a6b
SuguruOoki <suguru.oki.programming@gmail.com>
2017-11-26 04:14:14 +0900

    add click event start

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
- var insertClass = document.getElementsByClassName('chatInput__submitContainer');
- var div = document.createElement('div');
- div.textContent = 'ありがとう';
- document.body.insertBefore(div, insertClass.firstChild);
+var insertClass = document.getElementsByClassName('chatInput__submitContainer');
+var thanksbutton = '<div id="_thanks-send" class="chatInput__submit _cwBN button" role="button" tabindex="2" aria-disabled="false">ありがとう</div>';
+document.body.insertBefore(thanksbutton, insertClass.firstChild);
+
+var thanks_object = document.getElementById('_thanks-send');
+thanks_object.onclick = function() { alert("I was clicked!"); }; //こういうふうにイベントリスナーを設定してあると
+thanks_object.onclick();
+
