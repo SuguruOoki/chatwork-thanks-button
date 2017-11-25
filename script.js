d4349cf12a3eed16a00bb6632a461805c852e1f7
SuguruOoki <suguru.oki.programming@gmail.com>
2017-11-26 03:58:06 +0900

    add a thanks button

---
+++ script.js
@@ -0,0 +1,4 @@
+ var insertClass = document.getElementsByClassName('chatInput__submitContainer');
+ var div = document.createElement('div');
+ div.textContent = 'ありがとう';
+ document.body.insertBefore(div, insertClass.firstChild);
