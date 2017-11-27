1a49d9ed81c0ab3798843c8f6ed0919316eba7cb
SuguruOoki <suguru.oki.programming@gmail.com>
2017-11-27 18:37:06 +0900

    fix

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
- var insertClass = document.getElementsByClassName('chatInput__submitContainer');
- var div = document.createElement('div');
- div.textContent = 'ありがとう';
- document.body.insertBefore(div, insertClass.firstChild);
+var insertClass = document.getElementsByClassName('chatInput__submitContainer');
+var thanksbutton = '<div id="_thanks-send" class="chatInput__submit _cwBN button" role="button" tabindex="2" aria-disabled="false">Thanks!</div>';
+
+// divタグを作成して変数に代入する
+var thanksbutton = document.createElement("div");
+thanksbutton.setAttribute("class", "chatInput__submit _cwBN button");
+thanksbutton.setAttribute("id", "_thanks-send");
+thanksbutton.setAttribute("role","button");
+thanksbutton.innerHTML = "Thanks!";
+document.body.insertBefore(thanksbutton, insertClass.firstChild);
+
+//var thanks_object = document.getElementById('_thanks-send');
+thanksbutton.onclick = function() { alert("I was clicked!"); };
+thanksbutton.onclick();
+
