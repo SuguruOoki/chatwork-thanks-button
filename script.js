449ccd17d99986f5d9b94d7026e9e6a8467218be
hm_suguru_ohki <oki.suguru@hamee.co.jp>
2017-11-28 17:15:53 +0900

    fix a post process

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 var insertClass = document.getElementById('_sendButton');
-// divタグを作成して変数に代入する
 var thanksbutton = document.createElement("span");
 thanksbutton.className = "chatInput__submit _cwBN button" ;
 thanksbutton.setAttribute("id", "_thanks-send");
 thanksbutton.setAttribute("role","button");
 thanksbutton.innerHTML = "ありがとう!";
-insertClass.parentNode.insertBefore(thanksbutton, insertClass.parentNode.firstChild); //親クラス 入れる要素 基準となるタグ
+insertClass.parentNode.insertBefore(thanksbutton, insertClass.parentNode.firstChild);
+
+/**
+ * データをPOSTする
+ * @param String アクション
+ * @param Object POSTデータ連想配列
+ */
+function execPost(action, data) {
+ var form = document.createElement("form");
+ form.setAttribute("action", action);
+ form.setAttribute("method", "post");
+ form.style.display = "none";
+ document.body.appendChild(form);
+ // パラメタの設定
+ if (data !== undefined) {
+  for (var paramName in data) {
+   var input = document.createElement('input');
+   input.setAttribute('type', 'hidden');
+   input.setAttribute('name', paramName);
+   input.setAttribute('value', data[paramName]);
+   form.appendChild(input);
+  }
+ }
+ form.submit();
+}
 
 thanksbutton.onclick = function() {
     var sendText = document.getElementById('_chatText').value;
-    alert(sendText);
+    var senderId = document.getElementById('_myStatusIcon').childNodes[0].dataset.aid;
+    var url = '';
+    thanksPostData = {'senderId':senderId, 'senderText':sendText};
+    execPost(url,thanksPostData);
  };
