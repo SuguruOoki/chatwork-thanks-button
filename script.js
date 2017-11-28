7b26188333b791053e3d49a512cf3f9ad289db57
hm_suguru_ohki <oki.suguru@hamee.co.jp>
2017-11-28 12:28:48 +0900

    fix a url and element

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
- var insertClass = document.getElementsByClassName('chatInput__submitContainer');
- var div = document.createElement('div');
- div.textContent = 'ありがとう';
- document.body.insertBefore(div, insertClass.firstChild);
+var insertClass = document.getElementById('_chatSendToolbar');
+var div = document.createElement('span');
+div.classList.add("chatInput__submit");
+div.classList.add("_cwBN button");
+div.textContent = 'ありがとう';
+document.body.insertBefore(div, insertClass.firstChild);
