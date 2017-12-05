5c189091c42cb5495c7b617dd78d63eee1fd6973
hm_suguru_ohki <oki.suguru@hamee.co.jp>
2017-12-05 14:30:39 +0900

    add to find a response text

--- script.js
+++ script.js
@@ -5,47 +5,64 @@ thanksbutton.setAttribute("id", "_thanks-send");
 thanksbutton.setAttribute("role","button");
 thanksbutton.innerHTML = "ありがとう!";
 insertClass.parentNode.insertBefore(thanksbutton, insertClass.parentNode.firstChild);
+var req = new XMLHttpRequest();
 
-/**
- * データをPOSTする
- * @param String アクション
- * @param Object POSTデータ連想配列
+/*
+・TOの名前にニックネームが設定されていたら、正常に動作しない
+・「ありがとう！」のUIを「送信」の隣に置いて背景色を緑色などに変更しておく
+======================== ↑ 優先度 高
+・サーバーからエラーレスポンスが返ってきたら、管理者（柳）にご連絡くださいとアラートを表示する
+・返信もTOとして扱うようにする
+・TOが複数あった場合に、本文に2つ目以降のTOの文字列が含まれる
  */
-function execPost(action, data) {
- var form = document.createElement("form");
- form.setAttribute("action", action);
- form.setAttribute("method", "post");
- form.style.display = "none";
- document.body.appendChild(form);
 
- // パラメタの設定
- if (data !== undefined) {
-  for (var paramName in data) {
-   var input = document.createElement('input');
-   input.setAttribute('type', 'hidden');
-   input.setAttribute('name', paramName);
-   input.setAttribute('value', data[paramName]);
-   form.appendChild(input);
-  }
- }
- form.submit();
-}
 
 thanksbutton.onclick = function() {
+    thanksbutton.disabled = true;
     var sendText = document.getElementById('_chatText').value;
     var senderId = document.getElementById('_myStatusIcon').childNodes[0].dataset.aid;
     var url = 'http://127.0.0.1:3000/thanks/new';
+    var method = 'POST';
     var targetMessage = '';
+    var to_target = '';
+    var response_target = '';
+    var target = '';
 
+    // 送信先(To:)のユーザIDを取得
     if (sendText.match(/\[To:(.[0-9]+)\]/) !== null) {
-        target = sendText.match(/\[To:(.[0-9]+)\]/)[1];
-        sendText = sendText.match(/\[To:.+]さん\n(.+)?/s)[1];
+        to_target = sendText.match(/\To:([0-9]+)/sg)[1];
+        to_target =  to_target.slice(3);
+        // for (var ii = 0; ii < target.length; ii++) {
+        //     to_target[ii] = to_target[ii].slice(3);
+        // }
+
     }
 
+    // var target = to_target.concat(response_target).unique();
+    // To と 返信 どちらもあるときはToを優先
+    if (to_target !== '') {
+        target = to_target;
+    } else {
+        // 送信先(返信)を取得
+        if (sendText.match(/\[返信 aid\=(.[0-9]+) /sg) !== null) {
+            response_target = sendText.match(/\[返信 aid\=(.[0-9]+) /sg)[1];
+            response_target = response_target.slice(8);
+            // for (var jj = 0; jj < response_target.length; jj++) {
+            //     response_target[jj] = response_target[jj].slice(8);
+            // }
+        }
+        target = response_target;
+    }
+
+    sendText = sendText.match(/\[To:.+](.*)さん(.+)?/s);
     if (sendText === '↵') {
         sendText = '';
     }
 
-    thanksPostData = {'from_chatwork_id:':senderId, 'to_chatwork_id':target, 'message':sendText};
-    execPost(url,thanksPostData);
+    thanksPostData = 'from_chatwork_id='+senderId+'&to_chatwork_id='+target+'&message='+sendText;
+    // req.open(method, url, true);
+    // req.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
+    // req.send(thanksPostData);
+    // req.abort();
+    // document.getElementById('_chatText').value="";
  };
