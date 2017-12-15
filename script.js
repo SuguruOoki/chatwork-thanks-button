8517efaf3361be37d63df622b10db6132e1933fe
hm_suguru_ohki <oki.suguru@hamee.co.jp>
2017-12-15 14:04:03 +0900

    fix all

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-var insertClass = document.getElementById('_sendButton');
+var insertClass = document.getElementById('_groupCall');
 var thanksbutton = document.createElement("div");
-var req = new XMLHttpRequest();
 
 // 要素についての指定
 thanksbutton.className = "chatInput__submit button" ;
@@ -11,9 +10,19 @@ thanksbutton.setAttribute("role","button");
 thanksbutton.style.marginLeft="3px";
 thanksbutton.style.borderColor="#5cb85c";
 thanksbutton.style.background="#5cb85c";
-thanksbutton.innerHTML = "ありがとう!";
+thanksbutton.innerHTML = 'ありがとう!';
 insertClass.parentNode.insertBefore(thanksbutton, insertClass.parentNode.lastChild);
 
+
+function confirmOnClick() {
+    var txt;
+    var r = confirm("ありがとうを送信してもよろしいですか？");
+    if (r == true) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 function notificate(title,message_contents) {
     if(window.Notification != null) {
@@ -33,14 +42,15 @@ function notificate(title,message_contents) {
     }
 }
 
-// TODO サーバーからエラーレスポンスが返ってきたら、管理者（柳）にご連絡くださいとアラートを表示する
+// サーバーからエラーレスポンスが返ってきたら、管理者（柳）にご連絡くださいとアラートを表示する
 thanksbutton.onclick = function() {
-    var sendText = document.getElementById('_chatText').value;
+    var req = new XMLHttpRequest();
+    var sendText = "";
+    sendText = document.getElementById('_chatText').value;
+    var allMessage = sendText;
     var senderId = document.getElementById('_myStatusIcon').childNodes[0].dataset.aid;
-    var url = 'http://104.199.162.8:12390/thanks/new';
+    var url = 'https://dev-free01.next-engine.com:12390/thanks/new';
     var method = 'POST';
-
-    // TODO: 複数送信時にはこの辺りの初期化を変更する
     var targetMessage = '';
     var to_target = '';
     var response_target = '';
@@ -58,12 +68,7 @@ thanksbutton.onclick = function() {
         to_target = sendText.match(/\To:([0-9]+)/s);
         to_target = to_target[0];
         to_target =  to_target.slice(3);
-        // for (var ii = 0; ii < target.length; ii++) {
-        //     to_target[ii] = to_target[ii].slice(3);
-        // }
     }
-
-    // var target = to_target.concat(response_target).unique();
     // To と 返信 どちらもあるときはToを優先
     if (to_target !== '') {
         target = to_target;
@@ -72,15 +77,11 @@ thanksbutton.onclick = function() {
         // 送信先(返信)を取得
         response_target = sendText.match(/\[返信 aid\=(.[0-9]+) /s)[1];
         target = response_target;
-        // for (var jj = 0; jj < response_target.length; jj++) {
-        //     response_target[jj] = response_target[jj].slice(8);
-        // }
     } else {
         notificate('入力不足！','誰にありがとうを伝えたいですか？');
         return;
     }
 
-    // TODO: 複数でのToや返信の送信時の対応が必要
     if (sendText.match(/\さん(.+)?/s) !== null) {
         sendText = sendText.match(/\さん(.+)?/s)[1];
     }
@@ -89,15 +90,24 @@ thanksbutton.onclick = function() {
         sendText = '';
     }
 
+    var sendConfirm = confirmOnClick();
+
+    if (sendConfirm === false) {
+        notificate('送信中止','送信を中止しました！');
+        return;
+    }
+
     thanksPostData = 'from_chatwork_id='+senderId+'&to_chatwork_id='+target+'&message='+sendText;
+
     req.open(method, url, true);
     req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
-    // TODO: この辺りはサーバーのレスポンスによって対応を変える時の処理。2017/12/06 現在はまだ未完成。
     req.addEventListener('loadend', function() {
         if (req.status === 200) {
+            document.getElementById('_chatText').value = allMessage + '\n[ありがとう送信です！]';
+            const sendBtn = document.getElementById('_sendButton');
             notificate('送信成功！','ありがとうを送りました！');
-            document.getElementById('_chatText').value="";
+            sendBtn.click();
         } else {
             notificate('エラー！','正常に送信できませんでした！');
             return;
